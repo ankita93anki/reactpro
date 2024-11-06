76628db54f12338393b20a70cd27ff4487e77bf4
Ankita Singh <ankitasingh7639369@gmail.com>
2024-11-06 18:39:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let app_name = 'react app';
+  let className = 'App-header';
+  console.log('App')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={className}>
+        {app_name}
+        <Demo></Demo>
+      </div>
+    </div>
+  );
+}
+
+function Demo() {
+  return (
+    <div className="App">
+      <div className="App-header">Demo world</div>
     </div>
   );
 }
